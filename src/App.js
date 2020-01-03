4c10daec429ab53bc2f71aa75672b8b50ff067e0
Adam La Morre <alamorre@gmail.com>
2020-01-02 20:42:02 -0500

    ready for home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Checkbox } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Zopp
-        </a>
-      </header>
+    <div>
+      This is an app
+      <Checkbox>
+        Sign Me Up!
+      </Checkbox>
     </div>
   );
 }
